00ef0ea58e36a4ed087b151d8e454f801fecadac
sshu <sshu@ptc.com>
2022-08-04 10:10:22 +0800

    add flowers

--- src/pages/ComponentsLib/Grass.vue
+++ src/pages/ComponentsLib/Grass.vue
@@ -16,12 +16,12 @@ onMounted(() => {
     x: number
     y: number
   }
-
   interface PointB {
     x: number
     y: number
     theta: number
   }
+
   let start: PointA = {
     x: canvas.width / 2,
     y: canvas.height
@@ -29,8 +29,11 @@ onMounted(() => {
   let end: PointB = {
     x: canvas.width / 2,
     y: canvas.height - 20,
-    theta: PI * getRandomBetween(0.25, 0.75)
+    theta: PI * 0.5
   }
+
+  const tasks: Function[] = []
+
   function draw(start: PointA, end: PointB, random: number = 0) {
     ctx.beginPath()
     ctx.moveTo(start.x, start.y)
@@ -38,26 +41,57 @@ onMounted(() => {
     ctx.strokeStyle = '#9ca3af'
     ctx.stroke()
     if (start.x >= 500 || start.x <= 0 || start.y <= 0 || start.y >= 500) return
-    let randomAlpha = getRandomBetween(0.2, 0.8) * PI
-    let randomBeta = getRandomBetween(0.2, 0.8) * PI
-    while (end.theta === randomAlpha) {
-      randomAlpha = getRandomBetween(0.1, 0.5) * PI
-    }
-    while (end.theta === randomBeta) {
-      randomBeta = getRandomBetween(0.1, 0.5) * PI
-    }
+    let randomAlpha = end.theta + getRandomBetween(0.1, 0.2) * PI
+    let randomBeta = end.theta - getRandomBetween(0.1, 0.2) * PI
 
-    let length
+    let endLeft: boolean = true
+    let endRight: boolean = true
+
     if (getRandomBetween(0, 1) > random) {
-      length = getRandomBetween(10, 50)
-      draw(end, { x: end.x + length * Math.cos(randomAlpha), y: end.y - length * Math.sin(randomAlpha), theta: randomAlpha }, random + 0.07)
+      endLeft = false
+      tasks.push(() => {
+        const length = getRandomBetween(20, 30)
+        draw(end, { x: end.x + length * Math.cos(randomAlpha), y: end.y - length * Math.sin(randomAlpha), theta: randomAlpha }, random + 0.06)
+      })
     }
     if (getRandomBetween(0, 1) > random) {
-      length = getRandomBetween(10, 50)
-      draw(end, { x: end.x + length * Math.cos(randomBeta), y: end.y - length * Math.sin(randomBeta), theta: randomBeta }, random + 0.07)
+      endRight = false
+      tasks.push(() => {
+        const length = getRandomBetween(20, 30)
+        draw(end, { x: end.x + length * Math.cos(randomBeta), y: end.y - length * Math.sin(randomBeta), theta: randomBeta }, random + 0.06)
+      })
+    }
+    if (endLeft && endRight) {
+      drawCircle(end.x, end.y, 5)
     }
   }
+
+  function drawCircle(x: number, y: number, r: number) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.strokeStyle = '#b56271'
+    ctx.stroke();
+  }
+
   draw(start, end)
+
+  function oneStep() {
+    const thisTasks = [...tasks]
+    tasks.length = 0
+    thisTasks.forEach(fn => fn())
+  }
+
+  let frameCount = 0
+  function startDraw() {
+    requestAnimationFrame(() => {
+      frameCount++
+      if (frameCount % 2 === 0) {
+        oneStep()
+      }
+      startDraw()
+    })
+  }
+  startDraw()
 })
 </script>
 
